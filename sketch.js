b99735fa6e632ad5ba602c1ae59a8667ef6b098f
Vinayak-07 <75664275+Vinayak-07@users.noreply.github.com>
2021-02-04 16:54:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,5 @@
+function draw(){
+    text("COVID-19 is a disease caused by a new strain of coronavirus. ");
+    text("'CO' stands for corona, 'VI' for virus, and 'D' for disease.");
+    text("Formerly, this disease was referred to as '2019 novel coronavirus' or '2019-nCoV.");
+}
